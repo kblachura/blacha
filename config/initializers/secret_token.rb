945a583a1bfae144f64bc34a45f96f34e8ac6b16
Kamil Blachura <kamil.blachura@polcode.net>
2014-07-24 13:52:12 +0200

    Initilize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blacha::Application.config.secret_key_base = '7679099e9f843197b904ca8dc9c5d10e13d192292129774fce4e4766e1c2b3b1181344ffd6627ffee975fe3b6dda909ba72f87f181f7e61da4505928638b0fa0'
